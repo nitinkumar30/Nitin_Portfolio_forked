1af651781bf6b562b844a04a505e318a7dc21a74
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2022-07-30 16:57:42 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,35 +13,34 @@ const Main = () => {
             LET&apos;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&apos;m <span className='text-[#5651e5]'> Shabbir</span>
+            Hi, I&apos;m <span className='text-[#5651e5]'> Nitin</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Blockchain Developer</h1>
+          <h1 className='py-2 text-gray-700'>Automation Developer</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
           <span > I am a social geek interested in building and managing communities. Fast learner and adaptable to different project environments. Team player, but can also work solo.</span>
           <span> </span>
-          <span>Currently,I’m focused on building responsive front-end web applications while
-            learning back-end technologies</span>.
+          <span>Currently,I’m focused on building a project which'll help job seekers to find jobs according to their preferences by automating this task.</span>.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
-              href='https://www.linkedin.com/in/shabbiryk/'
+              href='https://www.linkedin.com/in/nitin30kumar/'
               
             >
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <FaLinkedinIn />
               </div> 
             </a>
-            <a href='https://github.com/shabbiryk' >
+            <a href='https://github.com/nitinkumar30/' >
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <FaGithub />
               </div>
             </a>
-            <Link href='/#contact'>
+            <Link href='mailto:nitinkumarpythonic@gmail.com'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <AiOutlineMail />
               </div>
             </Link>
-            <Link href=''>
+            <Link href='https://wa.me/919113797199'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <BsFillPersonLinesFill />
               </div>
